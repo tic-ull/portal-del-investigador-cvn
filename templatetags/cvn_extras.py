0835d9d062bf747d99d8e63292c8cbb65bb79601
Rayco Abad Martín <rabadmar@ull.edu.es>
2015-01-22 12:31:09 +0000

    Added License AGPL

--- templatetags/cvn_extras.py
+++ templatetags/cvn_extras.py
@@ -1,5 +1,27 @@
 # -*- encoding: UTF-8 -*-
 
+#
+#    Copyright 2014-2015
+#
+#      STIC-Investigación - Universidad de La Laguna (ULL) <gesinv@ull.edu.es>
+#
+#    This file is part of CVN.
+#
+#    CVN is free software: you can redistribute it and/or modify it under
+#    the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    CVN is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with CVN.  If not, see
+#    <http://www.gnu.org/licenses/>.
+#
+
 from cvn import settings as st_cvn
 from django.utils.safestring import mark_safe
 from django import template
