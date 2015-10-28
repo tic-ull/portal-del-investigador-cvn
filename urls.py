6415615ed93b06fd9580e3beb5a321fee362a94c
Hugo Jiménez <hjimeneh@ull.edu.es>
2015-10-28 14:32:35 +0000

    Fix in ull_report; PEP8 Fixes

--- urls.py
+++ urls.py
@@ -26,17 +26,21 @@ from django.conf.urls import patterns, url
 from django.http import HttpResponse
 from .settings import UNIVERSITY_REPORT_DB
 from .views import AdminReportsView, DownloadReportView, ReportsView
+import views
 
 urlpatterns = patterns(
     'cvn.views',
     url(r'^$', 'index', name='cvn'),
-    url(r'^ull_report/$', 'cvn.views.university_report',
+    url(r'^ull_report/$', views.university_report,
         {'year': UNIVERSITY_REPORT_DB}, name='ull_report'),
     url(r'^download/$', 'download_cvn', name='download_cvn'),
     url(r'^waiting/$', lambda r: HttpResponse(status=204), name='waiting'),
     url(r'^export_data/$', 'export_data_ull', name='export_data_ull'),
     url(r'^reports/$', ReportsView.as_view(), name='reports'),
     url(r'^admin_reports/$', AdminReportsView.as_view(), name='admin_reports'),
-    url(r'^reports/(?P<type>\w+)/(?P<code>[0-9]+)/(?P<year>[0-9]{4})/(?P<unit_type>\w+)$', DownloadReportView.as_view(), name='download_report'),
-    url(r'^reports/(?P<type>rcsv)/(?P<year>[0-9]{4})/(?P<unit_type>\w+)$', DownloadReportView.as_view(), name='download_report'),
+    url((r'^reports/(?P<type>\w+)/'
+         r'(?P<code>[0-9]+)/(?P<year>[0-9]{4})/(?P<unit_type>\w+)$'),
+        DownloadReportView.as_view(), name='download_report'),
+    url(r'^reports/(?P<type>rcsv)/(?P<year>[0-9]{4})/(?P<unit_type>\w+)$',
+        DownloadReportView.as_view(), name='download_report'),
 )
